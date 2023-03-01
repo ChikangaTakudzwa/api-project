1809eea5a2da695f73904ac5731571822662d181
ChikangaTakudzwa <chikangatakudzwa@outlook.com>
2023-03-01 21:53:21 +0000

    rm api-view decorators

--- project/api/views.py
+++ project/api/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics
 
 # Create your views here.
-@api_view(['GET', 'POST' , 'PUT', 'DELETE'])
+
 class BookView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-@api_view(['GET', 'POST' , 'PUT', 'DELETE'])
 class SingleBook(generics.RetrieveUpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
